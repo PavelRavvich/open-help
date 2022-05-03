7071bd92cb458b178f2d01fd3552d61fd78e483c
pavel <triodjangopiter@yandex.ru>
2022-05-03 19:10:54 +0300

    unit test launch reference example

--- story/src/test/java/com/openhelp/story/StoryApplicationTests.java
+++ story/src/test/java/com/openhelp/story/StoryApplicationTests.java
@@ -1,13 +1,19 @@
 package com.openhelp.story;
 
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
 
 @SpringBootTest
+@WebAppConfiguration
+@RunWith(SpringRunner.class)
 class StoryApplicationTests {
 
     @Test
     void contextLoads() {
+        StoryApplication.main(new String[] {});
     }
 
 }
