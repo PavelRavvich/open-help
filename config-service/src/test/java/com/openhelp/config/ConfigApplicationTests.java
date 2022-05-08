14a398b6d1f19c384a1007c24df13a548632813c
pavel <triodjangopiter@yandex.ru>
2022-05-08 18:34:39 +0300

    ref service naming

---
+++ config-service/src/test/java/com/openhelp/config/ConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.openhelp.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ group-service/src/main/java/com/openhelp/group/GroupApplication.java
@@ -0,0 +1,15 @@
+package com.openhelp.group;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class GroupApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GroupApplication.class, args);
+    }
+
+}
