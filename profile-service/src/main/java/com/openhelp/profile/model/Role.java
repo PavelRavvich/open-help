033789a717db10c195eb290601e80d3ff72f6613
pavel <triodjangopiter@yandex.ru>
2022-06-04 23:40:05 +0300

    ref

--- profile-service/src/main/java/com/openhelp/profile/model/Role.java
+++ profile-service/src/main/java/com/openhelp/profile/model/Role.java
@@ -7,7 +7,15 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
 import java.util.List;
 import java.util.Objects;
 
